0f7316d1defda5d730ed4fb8c89abb90b7f4ad89
now17 <now17@sgrsoft.com>
2026-05-13 12:48:18 +0900

    add SDK management scripts, migrate `vpe-react-native` to packaged dependency, and update `metro.config.js` to support dynamic peer dependency resolution

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,33 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
+const fs = require('fs');
 
 const projectRoot = __dirname;
 const sdkRoot = path.resolve(projectRoot, '../vpe-react-native');
+const sdkLink = path.resolve(projectRoot, 'node_modules/vpe-react-native');
+
+const isPortalLinked =
+	fs.existsSync(sdkRoot) && fs.existsSync(sdkLink) && fs.lstatSync(sdkLink).isSymbolicLink();
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [sdkRoot];
+if (isPortalLinked) {
+	const sdkPackage = require(path.join(sdkRoot, 'package.json'));
+	const peerDeps = Object.keys(sdkPackage.peerDependencies || {});
 
-const sdkPackage = require(path.join(sdkRoot, 'package.json'));
-const peerDeps = Object.keys(sdkPackage.peerDependencies || {});
-config.resolver.blockList = peerDeps.map(
-	(name) => new RegExp(`${sdkRoot.replace(/[/\\]/g, '[/\\\\]')}[/\\\\]node_modules[/\\\\]${name}[/\\\\].*`)
-);
+	config.watchFolders = [sdkRoot];
 
-config.resolver.extraNodeModules = peerDeps.reduce((acc, name) => {
-	acc[name] = path.join(projectRoot, 'node_modules', name);
-	return acc;
-}, {});
+	config.resolver.blockList = peerDeps.map(
+		(name) =>
+			new RegExp(
+				`${sdkRoot.replace(/[/\\]/g, '[/\\\\]')}[/\\\\]node_modules[/\\\\]${name}[/\\\\].*`
+			)
+	);
 
-config.resolver.disableHierarchicalLookup = false;
+	config.resolver.extraNodeModules = peerDeps.reduce((acc, name) => {
+		acc[name] = path.join(projectRoot, 'node_modules', name);
+		return acc;
+	}, {});
+}
 
 module.exports = config;
